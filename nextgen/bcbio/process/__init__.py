6fb3f91a5a68b0c47de10fefcd0d355f58b264b6
Kevin Ying <k.ying@garvan.org.au>
2012-12-13 18:34:23 +1100

    subprocess_logged

---
+++ nextgen/bcbio/process/__init__.py
@@ -0,0 +1,20 @@
+"""Allows subprocess calls to be autmatically logged
+   eg: subprocess_logged.check_call()
+"""
+import sys
+import inspect
+from bcbio.log import logger_subprocess
+import subprocess
+
+subprocess_logged = sys.modules[__name__]
+
+def logcall(func):
+    @wraps(func)
+    def _exec(*args, **kw):
+        logger_subprocess.info(" ".join(args[0]))
+        return func(*args, **kw)
+    return _exec
+
+for func, v in inspect.getmembers(subprocess, inspect.isroutine):
+    setattr(subprocess_logged, func, logcall(v))
+
